a6f53fcc635d26182773946e004615e22617cf13
askri bilel <askribilel09@gmail.com>
2019-08-27 21:10:41 +0100

    add services

--- src/app/service/SoftKill/soft-skill.service.ts
+++ src/app/service/SoftKill/soft-skill.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Settings} from '../../settings/settings';
+import {SoftSkill} from '../../models/SoftSkill';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SoftSkillService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  public findAllSoftSkill() {
+      return this.http.get(Settings.APP_URL + '/');
+  }
+
+  public createSoftSkill(softSkill: SoftSkill) {
+      return this.http.post(Settings.APP_URL + '/' , softSkill);
+  }
+
+  public findAllUsersSkills(id: number) {
+      return this.http.get(Settings.APP_URL + '/all' + id);
+  }
 }
